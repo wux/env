ac1164e968c596918430eee30f2ff2cf01558988
jwu <jwu@upad01.upthere.com>
2014-06-23 22:06:32 -0800

    link gen fix

--- run_ln.sh
+++ run_ln.sh
@@ -1,8 +1,8 @@
 pushd $HOME
-#ln -s env/.bash_profile .
-#ln -s env/.bashrc .
-#ln -s env/.emacs .
-#ln -s env/.emacs.d .
+ln -s env/.bash_profile .
+ln -s env/.bashrc .
+ln -s env/.emacs .
+ln -s env/.emacs.d .
 #ln -s env/bin .
 ln -s env/* .
 ln -s /ssd/a/jwu/ Local
